88047b241c7fa84cf8f0f5f23b9737d1913918f9
aryank87 <aryankumar91237@gmail.com>
2025-07-02 15:33:49 +0530

    creating a calculator project and updating in github

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator{
+
+    Calculator(){
+
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/ReadMe.txt
@@ -0,0 +1,4 @@
+I am making this project by watching a youtube video --
+https://www.youtube.com/watch?v=dfhmTyRTCSQ&t=112s
+
+And I am also using VCS (first time to know about git)
